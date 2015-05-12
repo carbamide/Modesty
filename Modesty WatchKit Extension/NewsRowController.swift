5101c47c6090dfa206f1de09b5a3a116b184f104
Josh Barrow <jbarrow84@me.com>
2015-05-12 14:28:44 -0500

    Extensive work on the Apple Watch app.

---
+++ Modesty WatchKit Extension/NewsRowController.swift
@@ -0,0 +1,13 @@
+//
+//  NewsRowController.swift
+//  Modesty
+//
+//  Created by Joshua Barrow on 5/10/15.
+//  Copyright (c) 2015 Jukaela Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class NewsRowController: NSObject {
+   
+}
